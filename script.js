b81969ea2031757db693000869a30316ebb26ab6
Aisha <fatimaaisha002@gmail.com>
2024-08-07 14:39:59 +0500

    new

---
+++ script.js
@@ -0,0 +1,17 @@
+// let text=""
+// const  fruits=["apple","mango","peach","strawberry"]
+// fruits.forEach(myFunction)
+// document.getElementById("demo").innerHTML=text
+
+// function myFunction(index,item) {
+//     text = text +index +" :"+item +"<br>"
+// }
+
+
+// let day=""
+// const months=["jan","feb","march","april","may","june","july","august"]
+// months.forEach(days)
+// document.getElementById("demo").innerHTML=day
+// function days(index,date){
+//     day=day+index+";"+date+"<br>"
+// }
