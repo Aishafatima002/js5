aa02454f2464713fee37cfb62a97b4674950cec2
Aisha <fatimaaisha002@gmail.com>
2024-08-28 15:51:13 +0500

    up

--- app.js
+++ app.js
@@ -6,10 +6,96 @@
 //     document.getElementById("swaps").src=newPic
 // }
 //when was approached
-function swapPic(){
-    document.getElementById("link")
-    linked.herf="https://www.w3schools.com/"
-}
+// function swapPic(){
+//     document.getElementById("link")
+//     linked.herf="https://www.w3schools.com/"
+// }
 
-function myFunction(x) {
-    x.style.background = "yellow";}
+// function myFunction(x) {
+//     x.style.background = "yellow";}
+
+
+
+
+// var plan1 = {
+//  name: "Basic",
+// space: 100,
+// transfer: 10,
+// pages: 10,
+// };
+// console.log(plan1,name)
+// delete plan1.space
+// console.log(plan1)
+
+// const person={
+
+// }
+// person.name="aisha"
+// person.age="18"
+// document.getElementById("demo").innerHTML=person.name+"is"+person.age+"yrs old"
+
+
+// console.log(person)
+
+// let student = {
+//     firstName: "Aisha",
+//     lastName: "Fatima",
+    
+//     fullName: function() {
+//         return this.firstName + " " + this.lastName;
+//     }
+// };
+
+// // Example usage
+// console.log(student.fullName()); // Outputs: "Aisha Fatima"
+
+
+// constructor function
+//  function Student(name,fatherName,age,cls){
+//         this.name=name;
+//         this.fatherName=fatherName;
+//         this.age=age;
+//         this.cls=cls;
+//  };
+//  let student1=new Student("AIZA"," Umer khan",12,"6")
+// console.log(student1)
+// let student2=new Student("MARIA","moiz ahemad",16,"11")
+// console.log(student2)
+// console.log(Student)
+
+
+// function Plan(name, price, space, transfer, pages) {
+//     this.doc = name; 
+//     this.grumpy = price; 
+//     this.sleepy = space; 
+//     this.bashful = transfer;
+//     this.sneezy =  pages;
+    
+//     }
+//     let plan1=new Plan("basic",100, 100,200,5)
+//     console.log(plan1)
+
+
+
+    
+// function Person(firstName,lastName,age,eye){
+//     this.firstName=firstName,
+//     this.lastName=lastName,
+//     this.age=age,
+//     this.eye=eye
+// }
+
+// const myFather=new Person("john","alle",43,"blue")
+// Person.prototype.nationality="English"
+// document.getElementById('demo').innerHTML="the nationality of my father is"+myFather.nationality;
+
+// function MY(name){
+//     this.name=name
+// }
+// MY.prototype.sayHello=function(){
+//     return `Hello my name is ${this.name}`
+// }
+
+// let aiza=new MY("aiza")
+
+// console.log(aiza.sayHello())
